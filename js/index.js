7f167f2f7fd730e532dc55658681ec7991c50117
F3D9 <federico.salgado04@gmail.com>
2023-10-25 14:14:05 -0300

    arreglue el bug

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ import { cargarEstructuras } from "./helpers/cargarEstructuras.js";
 import { cargarProductos } from "./helpers/cargarProductos.js";
 import { falsoLogin } from "./helpers/loginFalso.js";
 import { movimientoCarrusel } from "./helpers/movimientoCarrusel.js";
-
+import {cargarProductosHome} from "./helpers/cargarProductosHome.js";
 
 document.addEventListener("DOMContentLoaded", e => {
     cargarEstructuras(),
